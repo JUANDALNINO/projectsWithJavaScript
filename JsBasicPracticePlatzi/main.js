4903760f4c40716c147baf3e64ceb0333590e126
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-10-04 16:42:28 -0500

    Finish course

--- JsBasicPracticePlatzi/main.js
+++ JsBasicPracticePlatzi/main.js
@@ -4,6 +4,9 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuShopCar = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+const btnAll = document.querySelector('.btnAll');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -12,7 +15,7 @@ menuShopCar.addEventListener('click', toggleShopCar);
 function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
 
-    if(!isAsideClosed) {
+    if (!isAsideClosed) {
         aside.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
@@ -21,7 +24,7 @@ function toggleDesktopMenu() {
 function toggleMobileMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
 
-    if(!isAsideClosed) {
+    if (!isAsideClosed) {
         aside.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
@@ -40,21 +43,59 @@ function toggleShopCar() {
 
 const productList = [];
 
-productList.push ({
-    name:'Bicycle seat bag',
+productList.push({
+    name: 'Bicycle seat bag',
     price: 876,
     image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
-}); 
+});
 
-productList.push ({
-    name:'Bicycle helmet',
+productList.push({
+    name: 'Bicycle helmet',
     price: 1200,
     image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
 });
 
-productList.push ({
-    name:'Seat',
+productList.push({
+    name: 'Seat',
     price: 300,
     image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
 });
 
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCar = document.createElement('img');
+        productImgCar.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCar);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+btnAll.addEventListener('click', renderProducts(productList));
